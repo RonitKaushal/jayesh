6190b3d9a0701b6019976e0d17bc7e5ce81fc3fb
Ronit Kaushal <sparkpleazer@gmail.com>
2025-06-17 12:02:15 +0530

    Analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next"
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -65,6 +67,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Analytics />
         {children}
       </body>
     </html>
